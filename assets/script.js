14d3ee0193fb52cec885cacf22a53f907ac8bdc6
Rob V <rvans003@fiu.edu>
2021-01-28 23:35:20 -0500

    added automatic textBlock update based on localStorage key and values

--- assets/script.js
+++ assets/script.js
@@ -58,3 +58,14 @@ function hourVerification(){
 
 //calls the hourVerificaiton() function
 hourVerification();
+
+ //populates the textBlock of each time-block based on what is currently stored in localStorage
+ $("#hour9 .textBlock").val(localStorage.getItem("hour9"));
+ $("#hour10 .textBlock").val(localStorage.getItem("hour10"));
+ $("#hour11 .textBlock").val(localStorage.getItem("hour11"));
+ $("#hour12 .textBlock").val(localStorage.getItem("hour12"));
+ $("#hour13 .textBlock").val(localStorage.getItem("hour13"));
+ $("#hour14 .textBlock").val(localStorage.getItem("hour14"));
+ $("#hour15 .textBlock").val(localStorage.getItem("hour15"));
+ $("#hour16 .textBlock").val(localStorage.getItem("hour16"));
+ $("#hour17 .textBlock").val(localStorage.getItem("hour17"));
