7099810fa5193f31852eaf6a6ad4653fd021fbdf
Rob V <rvans003@fiu.edu>
2021-01-28 23:16:47 -0500

    added the saveBtn click functionality

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,16 @@
 var currentTime = luxon.DateTime.local();
 //displays the current date and time within the currentDay id <p> tag formated to DATETIME_MED
 $("#currentDay").text(currentTime.toLocaleString(luxon.DateTime.DATETIME_MED));
+
+//save button click functionality 
+$(".saveBtn").on("click", function (){
+   
+    //assigns the text value of the textBlock class of each timeblock to a variable called 'text'
+    var text = $(this).siblings(".textBlock").val();
+    
+    //assigns the id attribute value (which is the 'hourX) of each timeblock to a variable called 'time' 
+    var time = $(this).parent().attr("id");
+    
+    //sets local storage key 'time' and value 'text' for each timeblock
+    localStorage.setItem(time, text);
+})
